53638d1bca820898072f005bb196b6c7e7ffe826
Rahul Singh <rs.dangwal78223@gmail.com>
2026-04-11 23:08:03 +0530

    Add: subsets - submission-2

---
+++ Data Structures & Algorithms/subsets/submission-2.py
@@ -0,0 +1,11 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = [[]]
+        for num in nums:
+            tmp_result = []
+            for item in result:
+                tmp_result.append(item + [num])
+                #print(tmp_result)
+            result = result + tmp_result
+
+        return result
